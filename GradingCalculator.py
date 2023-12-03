099656376dcc582be3464f6e4f9269d3d937acb5
Mahedi Hasan <20787093+smahedih@users.noreply.github.com>
2023-12-03 23:43:44 +0100

    Update GradingCalculator.py

--- GradingCalculator.py
+++ GradingCalculator.py
@@ -1,22 +1,35 @@
 mark = int(input("Please enter your mark: "))
 
-if mark >= 80:
-    print("A+")
+if mark > 100:
+    print("Invalid input. Please enter mark between 0 and 100.")
+
+elif mark < 0:
+    print("Invalid input. Please enter mark between 0 and 100.")
+
+elif mark >= 80:
+    if mark <= 100:
+        print("A+")
 
 elif mark >= 70:
-    print("A")
+    if mark < 80:
+        print("A")
+
 
 elif mark >= 60:
-    print("A-")
+    if mark < 70:
+        print("A-")
 
 elif mark >= 50:
-    print("B")
+    if mark < 60:
+        print("B")
 
 elif mark >= 40:
-    print("C")
+    if mark < 50:
+        print("C")
 
 elif mark >= 33:
-    print("D")
+    if mark <= 40:
+        print("D")
 
 else:
     print("Failed.")
