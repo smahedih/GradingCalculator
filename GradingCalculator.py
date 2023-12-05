649f13d8c0ea1f6d5d9d9be6adcdac459aa778ed
Mahedi Hasan <20787093+smahedih@users.noreply.github.com>
2023-12-05 21:18:19 +0100

    Update GradingCalculator.py

--- GradingCalculator.py
+++ GradingCalculator.py
@@ -6,30 +6,23 @@ if mark > 100:
 elif mark < 0:
     print("Invalid input. Please enter mark between 0 and 100.")
 
-elif mark >= 80:
-    if mark <= 100:
-        print("A+")
+elif mark >= 80 and mark <= 100:
+    print("A+")
 
-elif mark >= 70:
-    if mark < 80:
-        print("A")
+elif mark >= 70 and mark < 80:
+    print("A")
 
+elif mark >= 60 and mark < 70:
+    print("A-")
 
-elif mark >= 60:
-    if mark < 70:
-        print("A-")
+elif mark >= 50 and mark < 60:
+    print("B")
 
-elif mark >= 50:
-    if mark < 60:
-        print("B")
+elif mark >= 40 and mark < 50:
+    print("C")
 
-elif mark >= 40:
-    if mark < 50:
-        print("C")
-
-elif mark >= 33:
-    if mark <= 40:
-        print("D")
+elif mark >= 33 and mark <= 40:
+    print("D")
 
 else:
-    print("Failed.")
+    print("Failed")
